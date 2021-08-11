2c2f9e477139a694985b63e680ea39ee47cfcb2a
christian-perez <christian.perez@centricityinsights.com>
2021-08-11 10:54:48 -0400

    added mac script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew cask install slack
+brew cask install sublime-text
+brew cask install visual-studio-code
+brew cask install iterm2
+brew cask install google-chrome
+brew install node
+brew install python
