0c0cfce5697be288c753fa6a6662c0efa0850854
christian-perez <christian.perez@centricityinsights.com>
2021-08-11 10:59:08 -0400

    edited mac script

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ brew cask install sublime-text
 brew cask install visual-studio-code
 brew cask install iterm2
 brew cask install google-chrome
+brew install zsh
 brew install node
 brew install python
