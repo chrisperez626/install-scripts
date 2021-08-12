3ba239fc87f5e158ee83e92c468e48e668a12c85
christian-perez <christian.perez@centricityinsights.com>
2021-08-12 12:13:38 -0400

    removed redundant packages for mac

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/usr/bin/ruby -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+brew install cask
 brew cask install slack
 brew cask install sublime-text
 brew cask install visual-studio-code
 brew cask install pycharm
 brew cask install iterm2
 brew cask install google-chrome
+brew cask install docker
+brew install htop
+brew install wget
 brew install git
 brew install zsh
 brew install node
-brew install python
